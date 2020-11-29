67f5ae6539130aa078277ec8c8df36899f1f2932
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-11-29 15:43:23 +0900

    Enable searchbar to get data from yelp

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import yelp from '../api/yelp';
 
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = ({ navigation }) => {
   const [term, setTerm] = React.useState('');
+  const [results, setResults] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState('');
+
+  /* helper */
+  const searchApi = async searchTerm => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: tesearchTermrm,
+          location: 'tokyo',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  React.useEffect(() => {
+    searchApi('pasta');
+  }, []);
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log('Term was submitted')}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
